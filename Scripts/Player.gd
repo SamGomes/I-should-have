b2ec2997459e0441234a2e75a126813b54ccf062
SamGomes <sam.s.gomes@sapo.pt>
2020-02-01 22:38:16 +0000

    fixing stuff

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -10,16 +10,25 @@ onready var sprite = $AnimatedSprite
 var y_velo = 0
 var facing_right = false
 
+var inputEnabled = true
+
+func disable_input():
+	inputEnabled = false
+func enable_input():
+	inputEnabled = true
+
 func _physics_process(delta):
+	if(!inputEnabled):
+		play_anim("idle")
+		return
 	var move_dir = 0
-	if Input.is_action_pressed("move_right"):
-		move_dir += 1
-	if Input.is_action_pressed("move_left"):
-		move_dir -= 1
-	
-	move_and_slide(Vector2(move_dir * MOVE_SPEED, y_velo), Vector2(0, -1))
 	
 	var grounded = is_on_floor()
+	if Input.is_action_pressed("move_right"):
+		move_dir += 1
+	elif Input.is_action_pressed("move_left"):
+		move_dir -= 1
+	
 	y_velo += GRAVITY
 	if grounded and Input.is_action_just_pressed("jump"):
 		y_velo = -JUMP_FORCE
@@ -27,6 +36,8 @@ func _physics_process(delta):
 		y_velo = 5
 	if y_velo > MAX_FALL_SPEED:
 		y_velo = MAX_FALL_SPEED
+	
+	move_and_slide(Vector2(move_dir * MOVE_SPEED, y_velo), Vector2(0, -1))
 	
 	if facing_right and move_dir < 0:
 		flip()
--- Scripts/StartScene.gd
+++ Scripts/StartScene.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
 onready var anim_player = $AnimationPlayer
+onready var player = $Player
 
 func _on_start_trigger_enter(body):
 #	print("aaaaaaaaaaaaaaaaaaa")
 	anim_player.play("CameraStartAnim")
+	player.disable_input()
 	yield(get_tree().create_timer(anim_player.get_current_animation_length()), "timeout") #wait() in GDscript
+	player.enable_input()
 	GameManager.changeMemory(0,0,0)
 #	GameManager.changeTeste()
