42f3227a4d76e184ba725dd650933da67a990d51
SamGomes <sam.s.gomes@sapo.pt>
2020-02-01 18:49:51 +0000

    merged with start scene...

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -16,28 +16,22 @@ func _ready():
 	submemories_l1_search.append("res://Scenes/Level1/Level1_search_2.tscn")	
 	memories.append([["res://Scenes/Hallway.tscn"],[]])
 	memories.append([submemories_l1_search,["res://Scenes/Level1/Level1_puzzle.tscn"]])
-#	memories.append(["res://Scenes/Level2_search.tscn","res://Scenes/Level2_puzzle.tscn"])
-#	memories.append(["res://Scenes/Level3_search.tscn","res://Scenes/Level3_puzzle.tscn"])
-	changeMemory(0,0,0)
+	
+	loadStartMenu()
 
 func loadStartMenu():
-	get_tree().change_scene("res://Scenes/StartScene.tscn")
+	return get_tree().change_scene("res://Scenes/StartScene.tscn")
 
 func changeMemory(levelNum, levelPart, type): #type=> 0:search, 1:puzzle
 	currMemory = memories[levelNum][type][levelPart]
 #	queue_free()
 #	var scene = load(currMemory).instance()
 #	add_child(scene)
-	get_tree().change_scene(currMemory)	
+	return get_tree().change_scene(currMemory)
 
 func changeTeste():
-	get_tree().change_scene("res://Scenes/Level1/Minigame1.tscn")
+	return get_tree().change_scene("res://Scenes/Level1/Minigame1.tscn")
 
-#func getPaper1():
-#	papers=papers+1
-#	get_node("Paper1").queue_free() 
-		
-	
 	
 	
 	
